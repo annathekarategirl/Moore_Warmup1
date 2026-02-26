f0cd165caaff295cfc8d16caef9fa388391d7b16
Anna Moore <annathekarategirl@gmail.com>
2026-02-26 14:11:32 -0500

    d

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 var sceneNumber=1
+function advanceScene(){
+    img=document.getElementById("img")
+    cap=document.getElementById("cap")
+    switch(sceneNumber){
+        case 1:
+            img.src="images/forest2.jpg"
+            cap.textContent="Lost."
+            sceneNumber++
+            break;
+        case 2:
+            img.src="images/forest3.jpg"
+            cap.textContent="Looking up. You are stuck here."
+            document.getElementById("ending").style.display=true
+            document.getElementById("button").disabled=true
+            break;
+    }
+}
+document.getElementById("advance").onclick=advanceScene
